bb883a453621de60ac6fdae835bb04382745d0ca
snie <snie1337@hotmail.com>
2017-10-16 23:37:27 +0200

    small fixes

---
+++ skeleton.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python2.7
+# USI - Università della svizzera italiana
+#
+# Machine Learning - Fall 2017
+#
+# Assignment 1: Neural Networks
+# Code Skeleton
+
+import numpy as np
+import plotters as plt
+from part1 import run_part1
+from Part2 import run_part2
+
+# run_part1()
+run_part2()
+
+
